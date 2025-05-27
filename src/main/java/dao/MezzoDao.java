58c4fed57fe25bbc676bf680a5d417002e217418
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-05-27 14:10:22 +0200

    mezzoDao

---
+++ src/main/java/dao/MezzoDao.java
@@ -0,0 +1,35 @@
+package dao;
+
+import entites.Mezzo;
+import jakarta.persistence.EntityManager;
+
+public class MezzoDao {
+
+    private EntityManager em;
+
+    public MezzoDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Mezzo mezzo){
+        em.getTransaction().begin();
+        em.persist(mezzo);
+        em.getTransaction().commit();
+    }
+
+    public Mezzo getById(Long id){
+        return em.find(Mezzo.class, id);
+    }
+
+    public void remove(Long id){
+        Mezzo mezzo = getById(id);
+
+        if(mezzo != null){
+            em.getTransaction().begin();
+            em.remove(mezzo);
+            em.getTransaction().commit();
+        } else {
+            System.out.println("Il mezzo " + mezzo + " non esiste");
+        }
+    }
+}
