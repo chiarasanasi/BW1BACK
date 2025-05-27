119459276ab046efc64db629ea4ea5cdb6626e8f
Roberto2ciancio <ciancioroberto181@gmail.com>
2025-05-27 16:20:32 +0200

    save

---
+++ src/main/java/dao/UtenteDao.java
@@ -0,0 +1,45 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import model.Utente;
+
+public class UtenteDao {
+
+    private EntityManager em;
+
+    public UtenteDao(EntityManager em) {
+        this.em = em;
+    }
+
+    // Salva un utente
+    public void salva(Utente utente) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(utente);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    // Ottieni un utente tramite ID
+    public Utente get(Long id) {
+        return em.find(Utente.class, id);
+    }
+
+    // Rimuovi un utente
+    public void rimuovi(Utente utente) {
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.remove(em.contains(utente) ? utente : em.merge(utente));
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+}
