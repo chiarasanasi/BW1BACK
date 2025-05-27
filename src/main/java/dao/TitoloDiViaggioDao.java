3a563dc363339756a33f902de1cec910487ccefa
Roberto Albergo <ralbergo7@gmail.com>
2025-05-27 18:01:23 +0200

    metodi eseguiti(TitoloDiViaggioDao)

--- src/main/java/dao/TitoloDiViaggioDao.java
+++ src/main/java/dao/TitoloDiViaggioDao.java
@@ -1,7 +1,14 @@
 package dao;
 
+import entites.Abbonamento;
+import entites.Biglietto;
 import entites.TitoloDiViaggio;
+import enumeration.Vidimazione;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class TitoloDiViaggioDao {
 
@@ -34,4 +41,45 @@ public class TitoloDiViaggioDao {
             System.out.println("Il titolo di viaggio con ID " + id + " non esiste");
         }
     }
-}
+
+        public Long numeroDiBigliettiInUnDatoPeriodo (LocalDate inizio, LocalDate fine){
+
+            TypedQuery<Long> query = em.createQuery(
+                    "SELECT COUNT(b) FROM Biglietto b WHERE b.dataEmissione BETWEEN :inizio AND : fine",
+                    Long.class);
+            query.setParameter("inizio", inizio);
+            query.setParameter("fine", fine);
+            return query.getSingleResult();
+        }
+
+        public boolean controlloValiditaAbbonamentoTramiteIdTessera (Long idTessera, LocalDate oggi){
+            TypedQuery<Abbonamento> query = em.createQuery(
+                    "SELECT a FROM Abbonamento a WHERE a.tessera.id = :idTessera AND a.dataScadenza >= :oggi",
+                    Abbonamento.class);
+            query.setParameter("idTessera", idTessera);
+            query.setParameter("oggi", oggi);
+            return !query.getResultList().isEmpty();
+
+        }
+
+        public List<Biglietto> ricercaBiglietti (Long idMezzo){
+            TypedQuery<Biglietto> query = em.createQuery(
+                    "SELECT b FROM Biglietto b WHERE b.mezzo.id = :idMezzo",
+                    Biglietto.class);
+            query.setParameter("idMezzo", idMezzo);
+            return query.getResultList();
+
+        }
+
+        public List<Biglietto> ricercaBigliettiVidimatiPerPeriodo (LocalDate inizio, LocalDate fine){
+            return em.createQuery(
+                            "SELECT b FROM Biglietto b WHERE b.vidimazione = :stato AND b.dataEmissione BETWEEN :inizio AND :fine",
+                            Biglietto.class)
+                    .setParameter("stato", Vidimazione.VIDIMATO)
+                    .setParameter("inizio", inizio)
+                    .setParameter("fine", fine)
+                    .getResultList();
+        }
+    }
+
+
