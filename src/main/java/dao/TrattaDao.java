dd4b339e1a6d738611e50b53129f3e22ee68a5f7
Silvia <silvia8gasparini@gmail.com>
2025-05-27 15:54:28 +0200

    corrections

--- src/main/java/dao/TrattaDao.java
+++ src/main/java/dao/TrattaDao.java
@@ -4,27 +4,22 @@ import entites.Tratta;
 import jakarta.persistence.EntityManager;
 
 public class TrattaDao {
-
     private EntityManager em;
-
     public TrattaDao(EntityManager em) {
         this.em = em;
     }
 
     public void save(Tratta tratta) {
         em.getTransaction().begin();
-        if (tratta.getId() == null) {
-            em.persist(tratta);
-        } else {
-            em.merge(tratta);
-        }
+        em.persist(tratta);
         em.getTransaction().commit();
+        System.out.println("La tratta " + tratta.getId() + " è stato aggiunta.");
     }
 
     public Tratta getById(Long id) {
         return em.find(Tratta.class, id);
     }
-    
+
     public void remove(Long id) {
         Tratta tratta = getById(id);
 
